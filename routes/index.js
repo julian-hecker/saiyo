2b283e32c4ffa6d9744e68ebfcf2404531b1d841
julian-hecker <julianheckerdev@gmail.com>
2020-08-11 00:30:17 -0400

    create resume compilation and downloading

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('homepage', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    return res.render('index');
 });
 
 router.get('/form', (req, res) => {
-  res.render('form')
-})
+    return res.render('form');
+});
 
-'http://localhost:3000/'
+router.get('/preview', (req, res) => {
+    return res.sendFile(compileResume({ title: 'reee' }));
+});
+
+router.get('/download', (req, res) => {
+    return res.download(compileResume({ title: 'reee' }));
+});
+
+function compileResume(data) {
+    const hb = require('handlebars');
+    const fs = require('fs');
+    const path = require('path');
+
+    const templateUrl = path.join(__dirname, '../views/template.hbs');
+    const template = fs.readFileSync(templateUrl, {
+        encoding: 'utf-8',
+        flag: 'r',
+    });
+    const compiledUrl = path.join(__dirname, '../temp/resume.html');
+    const compiled = hb.compile(template);
+    const html = compiled(data);
+
+    fs.writeFileSync(compiledUrl, html);
+
+    return compiledUrl;
+}
 
 module.exports = router;
