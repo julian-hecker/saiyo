653a453d16b32e8399d6201f3cc283d616003b4f
mnai01 <imatlak03@gmail.com>
2020-08-11 04:43:19 -0400

    removed excess white space

--- util/parse.js
+++ util/parse.js
@@ -7,34 +7,49 @@ const pdf = require('pdf-parse');
 let dataBuffer = fs.readFileSync('./Scott_Simko_Resume.pdf');
 
 pdf(dataBuffer).then(function (data) {
-  // number of pages
-  console.log(data.numpages);
-  // number of rendered pages
-  console.log(data.numrender);
-  // PDF info
-  console.log(data.info);
-  // PDF metadata
-  console.log(data.metadata);
-  // PDF.js version
-  // check https://mozilla.github.io/pdf.js/getting_started/
-  console.log(data.version);
-  // PDF text
-  //   console.log(data.text);
+  // // number of pages
+  // console.log(data.numpages);
+  // // number of rendered pages
+  // console.log(data.numrender);
+  // // PDF info
+  // console.log(data.info);
+  // // PDF metadata
+  // console.log(data.metadata);
+  // // PDF.js version
+  // // check https://mozilla.github.io/pdf.js/getting_started/
+  // console.log(data.version);
+  // //  PDF text
+  // console.log(data.text);
 
-  temp = data.text;
-  console.log(temp.replace(/(\n)/g, ';').split(';'));
+  let temp = data.text;
   arrayTemp = temp.replace(/(\n)/g, ';').split(';');
 
-  //   for (i = 0; i < arrayTemp.length; i++) {
-  //     if (arrayTemp[i] === ' ' || '   ') {
-  //       console.log(arrayTemp[i] + ' ' + true);
-  //       arrayTemp.splice(i, 1);
-  //     }
-  //   }
+  arraydel = [];
 
   for (i = 0; i < arrayTemp.length; i++) {
-    console.log(
-      'this is content' + ' ' + arrayTemp[i] + (arrayTemp[i] === /\s/g)
-    );
+    if (arrayTemp[i].replace(/\s/g, '') === '' || arrayTemp[i] === '') {
+      arraydel.push(i);
+    }
   }
+
+  for (i = 0; i < arraydel.length; i++) {
+    arrayTemp.splice(arraydel[i] - i, 1);
+  }
+
+  let obj = {
+    Name: '',
+    Experience: '',
+    date: '',
+    desc: '',
+  };
+  console.log(arrayTemp);
+
+  for (i = 0; i < arrayTemp.length; i++) {
+    if (obj.Name === '' && i == 0) obj.Name = arrayTemp[i];
+    if (arrayTemp[i].match(/\w+(?=\s+EXPERIENCE)/gi) && obj.Experience === '') {
+      obj.Experience = arrayTemp[i + 1];
+      obj.desc = arrayTemp[i + 2];
+    }
+  }
+  console.log(obj);
 });
