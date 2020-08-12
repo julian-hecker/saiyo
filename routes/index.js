4a1c71fd81a394a563102400d01c6f3a94be8c61
mnai01 <imatlak03@gmail.com>
2020-08-12 02:50:41 -0400

    parse function moved to upload route

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,76 @@
 const express = require('express');
 const router = express.Router();
-
+const fs = require('fs');
 const upload = require('express-fileupload');
-
-var parseResume = require('../util/parse.js')
+var parseResume = require('../util/parse.js');
+const pdf = require('pdf-parse');
+const ResumeParser = require('simple-resume-parser');
+const resume = new ResumeParser('../temp/resume.txt');
 
 router.get('/', function (req, res, next) {
-    return res.render('index');
+  return res.render('index');
 });
 
 router.get('/upload', (req, res) => {
-    return res.render('upload');
+  return res.render('upload');
 });
 
 router.post('/upload', upload(), (req, res) => {
-    // https://www.npmjs.com/package/express-fileupload
+  // https://www.npmjs.com/package/express-fileupload
+  //   req.files.resume
+  pdf(req.files.resume).then(function (data) {
+    console.log(Object.entries(data));
+    fs.writeFileSync('../temp/resume.txt', data.text, () => {
+      console.log('file written');
+    });
 
-    parseResume(req.files.resume);
-
-    return res.render('verify');
+    resume
+      .parseToJSON()
+      .then((data) => {
+        console.log(data);
+        return res.json(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+  //   return res.render('verify');
 });
 
 // render verify with details from analzed form
 router.post('/verify', (req, res) => {
-    res.render('verify', {});
+  res.render('verify', {});
 });
 
 // post with query params, render select
 
 router.get('/preview/:template', (req, res) => {
-    const template = 'template' + req.params.template;
-    return res.sendFile(compileResume(template, { title: 'reee' }));
+  const template = 'template' + req.params.template;
+  return res.sendFile(compileResume(template, { title: 'reee' }));
 });
 
 router.get('/download/:template', (req, res) => {
-    const template = 'template' + req.params.template;
-    return res.download(
-        compileResume(template, { title: 'reee' }),
-        'index.html',
-    );
+  const template = 'template' + req.params.template;
+  return res.download(compileResume(template, { title: 'reee' }), 'index.html');
 });
 
 function compileResume(template, data) {
-    const hb = require('handlebars');
-    const fs = require('fs');
-    const path = require('path');
+  const hb = require('handlebars');
+  const fs = require('fs');
+  const path = require('path');
 
-    const templateUrl = path.join(
-        __dirname,
-        `../views/${template}.hbs`,
-    );
-    const templateFile = fs.readFileSync(templateUrl, {
-        encoding: 'utf-8',
-        flag: 'r',
-    });
-    const compiledUrl = path.join(__dirname, '../temp/resume.html');
-    const compiled = hb.compile(templateFile);
-    const html = compiled(data);
+  const templateUrl = path.join(__dirname, `../views/${template}.hbs`);
+  const templateFile = fs.readFileSync(templateUrl, {
+    encoding: 'utf-8',
+    flag: 'r',
+  });
+  const compiledUrl = path.join(__dirname, '../temp/resume.html');
+  const compiled = hb.compile(templateFile);
+  const html = compiled(data);
 
-    fs.writeFileSync(compiledUrl, html);
+  fs.writeFileSync(compiledUrl, html);
 
-    return compiledUrl;
+  return compiledUrl;
 }
 
 module.exports = router;
