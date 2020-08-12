223f28486d9a64316a7a3b68d08bfc23adf5dba2
Scott Simko <scottasimko@gmail.com>
2020-08-12 01:38:10 -0400

    Changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var JSONobj = require('../util/parse.js')
+
 router.get('/', function (req, res, next) {
     return res.render('index');
 });
@@ -9,10 +11,15 @@ router.get('/upload', (req, res) => {
     return res.render('upload');
 });
 
+router.get('/verify', (req, res) => {
+  return res.render('verify', );
+});
+
 router.post('/form', (req, res) => {
     console.log('reee');
     res.end();
 });
+
 
 router.get('/preview/:template', (req, res) => {
     const template = 'template' + req.params.template;
